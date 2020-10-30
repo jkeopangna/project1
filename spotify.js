8d203d5eb591e15f67426558a2401a0c11452b20
IronJon <lang.jeffrey@gmail.com>
2020-10-29 21:24:42 -0600

    removed extraneous comments from spotify.js

--- spotify.js
+++ spotify.js
@@ -24,14 +24,3 @@ fetch("https://accounts.spotify.com/api/token", {
   .then(function (data) {
     console.log(data);
   });  
-  //current forecast
-  // city name, date, an icon w/ conditions, temperature, humidity, wind speed and uv index ( use css styles and class for color coding for favorable, normal, unfavorable)
-
-  //future 5 day forecast
-  //date, icon for conditions, temperature, and humidity
-
-  
-
-//   18c8e29c5116333e08aef89074ae53f7
-
-// curl -X GET "https://api.spotify.com/v1/search?q=tania%20bowra&type=artist" -H "Authorization: Bearer afd7da86e10e4f16b9fbb4edf29ca1a0"
